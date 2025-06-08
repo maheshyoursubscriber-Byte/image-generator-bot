aa368688164f7fc5e4f85860ba72d65975800afe
maheshyoursubscriber-Byte <maheshwasted@gmail.com>
2025-06-08 12:32:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const axios = require("axios");
 const TelegramBot = require("node-telegram-bot-api");
 const fetch = require("node-fetch");
 
-const token = "BOT_TOKEN";
-const admin = "USERID";
-const DATABASE_URL = "DATABASE_URL"; 
-const WEBHOOK_URL = "VERCEL_URL";
+const token = "7551215811:AAEyLxSw52NmAEls3xSWBGYqxiftDXoCWsY";
+const admin = "6843751342";
+const DATABASE_URL = "https://mahesh-firebase-real-time-data-default-rtdb.firebaseio.com"; 
+const WEBHOOK_URL = "https://image-generator-bot-omega.vercel.app/";
 
 const bot = new TelegramBot(token, { webHook: { port: false } });
 const app = express();
@@ -118,11 +118,11 @@ app.post("/", async (req, res) => {
       const caption =
         "*👆 Here Is Your Generated Image\n\n💭 Your Prompt:*\n`" +
         msg.text +
-        "`\n\n*🧑‍💻 Created By:* [BOTNAME](https://telegram.dog/BOT_USERNAME)";
+        "`\n\n*🧑‍💻 Created By:* [BOTNAME](https://telegram.dog/IMGINATOR_BOT)";
       const keyboard = {
         reply_markup: {
           inline_keyboard: [
-            [{ text: "🧑‍💻 Developer", url: "https://telegram.dog/DEVELOPER" }]
+            [{ text: "🧑‍💻 Developer", url: "https://telegram.dog/Saurabh Yadav" }]
           ]
         },
         parse_mode: "Markdown",
